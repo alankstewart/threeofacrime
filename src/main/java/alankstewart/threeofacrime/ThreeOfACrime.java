da82b65bcf96ae0ff9c6669a490e9bbbba663ee9
Alan Stewart <alankstewart@gmail.com>
2015-03-04 20:31:29 +1100

    Created standalone class to play a game

--- src/main/java/alankstewart/threeofacrime/ThreeOfACrime.java
+++ src/main/java/alankstewart/threeofacrime/ThreeOfACrime.java
@@ -3,7 +3,6 @@ package alankstewart.threeofacrime;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -15,53 +14,6 @@ public final class ThreeOfACrime implements Iterable<SuspectCard> {
 
     public ThreeOfACrime() {
         suspectCards = StreamSupport.stream(spliterator(), false).collect(toList());
-    }
-
-    public static void main(final String[] args) {
-        final ThreeOfACrime threeOfACrime = new ThreeOfACrime();
-        System.out.println("Enter three unique suspects and the number of matched suspects separated by commas or 'q' to quit");
-        final Scanner console = new Scanner(System.in);
-        while (console.hasNextLine()) {
-            final String line = console.nextLine();
-            if (line.equals("q")) {
-                break;
-            }
-
-            final String[] tokens = line.split(",");
-            if (tokens.length == 4) {
-                final int matches = Integer.parseInt(tokens[3]);
-                if (matches < 0 || matches > 2) {
-                    System.out.println("Must enter 0, 1, or 2 for the number of matches");
-                    continue;
-                }
-
-                final SuspectCard suspectCard;
-                try {
-                    suspectCard = SuspectCard.of(tokens[0], tokens[1], tokens[2]);
-                } catch (final IllegalArgumentException e) {
-                    System.out.println(e.getMessage());
-                    continue;
-                }
-
-                switch (matches) {
-                    case 0:
-                        threeOfACrime.matchZeroSuspects(suspectCard);
-                        break;
-                    case 1:
-                        threeOfACrime.matchOneSuspect(suspectCard);
-                        break;
-                    case 2:
-                        threeOfACrime.matchTwoSuspects(suspectCard);
-                        break;
-                }
-                threeOfACrime.printSuspectCards();
-                if (threeOfACrime.getSuspectCards().size() == 1) {
-                    break;
-                }
-            } else {
-                System.out.println("Must enter three suspects and number of matches");
-            }
-        }
     }
 
     @Override
