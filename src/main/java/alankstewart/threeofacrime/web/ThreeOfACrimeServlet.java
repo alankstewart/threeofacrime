18db3b62e835c31a2fc93c952e1a66341f03e429
Alan Stewart <alankstewart@gmail.com>
2015-03-04 22:44:16 +1100

    Added web artifacts

---
+++ src/main/java/alankstewart/threeofacrime/web/ThreeOfACrimeServlet.java
@@ -0,0 +1,18 @@
+package alankstewart.threeofacrime.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by alanstewart on 4/03/15.
+ */
+public class ThreeOfACrimeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
